d7b35ab2cf409f7941c3a199f809470d5320d406
DNC <michael.carpino@dncsrl.com>
2020-09-30 15:32:18 +0200

    Recreated workstations.dart because of an unknown error

--- lib/presentation/widgets/workstations.dart
+++ lib/presentation/widgets/workstations.dart
@@ -1,23 +1,23 @@
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:where_am_i/core/utils/constants.dart';
 import 'package:where_am_i/domain/entities/workstation.dart';
 
 class Workstations extends StatefulWidget {
-  const Workstations({Key key,
-    @required this.quantity,
-    @required this.columnsNumber,
-    this.columnsSpacing = 0,
-    @required this.workstations,
-    @required this.startingIndex})
-      : super(key: key);
-
   final int quantity;
   final int columnsNumber;
   final double columnsSpacing;
   final List<Workstation> workstations;
   final int startingIndex;
+
+  const Workstations(
+      {Key key,
+        @required this.quantity,
+        @required this.columnsNumber,
+        this.columnsSpacing = 0,
+        @required this.workstations,
+        @required this.startingIndex})
+      : super(key: key);
 
   @override
   _WorkstationsState createState() => _WorkstationsState();
@@ -47,14 +47,11 @@ class _WorkstationsState extends State<Workstations> {
     );
   }
 
-   String getText(int gridIndex) {
+  String getText(int gridIndex) {
     if (widget.workstations == null) {
       return "";
     } else {
       var workstationCode = gridIndex;
-      */
-/*in the older app, room 26 b and room 24 indexes were increasing in vertical order
-        instead of horizontal, so here they are mapped to match the original position*//*
 
       //room 26b
       if (gridIndex <= 18) {
@@ -65,7 +62,7 @@ class _WorkstationsState extends State<Workstations> {
         workstationCode = getOldWorkstationCodeFor24[gridIndex];
       }
       var workstationOfIndex = widget.workstations.firstWhere(
-          (element) => element.codeWorkstation == workstationCode.toString(),
+              (element) => element.codeWorkstation == workstationCode.toString(),
           orElse: () => null);
       return workstationOfIndex != null
           ? workstationOfIndex.resourceSurname
@@ -73,4 +70,3 @@ class _WorkstationsState extends State<Workstations> {
     }
   }
 }
-*/
